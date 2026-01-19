29a938ebd1cc03158f01696799c6c4579a17ef28
Madalei <rizzolioestelle@gmail.com>
2026-01-19 16:37:34 +0100

    update router to include POSTs e.p

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,13 +1,15 @@
 from os import system
 
 from fastapi import APIRouter
-from app.api import commands, queries
+from . import commands, queries
 from app.api.queries import binance
 from app.api.queries import bots
+from app.api.commands import bots
 
 router = APIRouter()
 
 # Include all routes
 router.include_router(queries.bots.router, tags=["queries"])
+router.include_router(commands.bots.router, tags=["commands"])
 router.include_router(queries.binance.router, tags=["queries"])
 #router.include_router(adapters.router, prefix="/system", tags=["system"])

1710765f45384ee25bf3e6dd88b9265f8a7bb1d1
Madalei <rizzolioestelle@gmail.com>
2026-02-03 12:44:00 +0100

    Set up exchange platform to Binance at boot

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api.router import router
 from app.config import applicationSettings
+from app.infrastructure.adapters.binance_adapter import BinanceAdapter
 from app.services.order_service import OrderService
 from app.bots.bot_manager import BotManager
 
@@ -13,18 +14,24 @@ async def lifespan(app: FastAPI):
     Lifespan context manager for startup and shutdown events.
     """
     # Startup
-    exchange = None  # your CCXT client
+    exchange = BinanceAdapter(
+        api_key=applicationSettings.binance_keys.api_key,
+        secret=applicationSettings.binance_keys.secret,
+        sandbox=True
+    )
     order_service = OrderService()
     bot_manager = BotManager(exchange=exchange, order_service=order_service)
 
     # Store in app.state
     app.state.bot_manager = bot_manager
+    app.state.exchange = exchange
 
     yield  # Application runs here
 
     # Shutdown (cleanup)
     # e.g., close connections, stop bots gracefully
     # await bot_manager.shutdown()
+    await exchange.close()
 
 
 app = FastAPI(title="Botox API", lifespan=lifespan)
@@ -32,30 +39,3 @@ app = FastAPI(title="Botox API", lifespan=lifespan)
 # Importer toutes les routes définies dans router.py
 app.include_router(router)
 
-
-
-#
-# app = FastAPI(title="Botox API")
-#
-# # Importer toutes les routes définies dans router.py
-# app.include_router(router)
-#
-# @app.on_event("startup")
-# async def startup_event():
-#     """
-#     Code executed once when the app starts.
-#     Initialize singleton services like BotManager.
-#     """
-#     # Binance client (async)
-#     #exchange = ...  # your CCXT client
-#     exchange = None
-#     #binance_service = BinanceService()
-#
-#     # OrderService internally manages repository/sessions
-#     order_service = OrderService()
-#
-#     # BotManager singleton, holds all running bots in memory
-#     bot_manager = BotManager(exchange=exchange, order_service=order_service)
-#
-#     # Store singleton in app.state
-#     app.state.bot_manager = bot_manager

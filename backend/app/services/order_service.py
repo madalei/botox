ecc2c1157e2ceb571ff8567f173c4839809e98c7
Madalei <rizzolioestelle@gmail.com>
2026-03-02 12:07:23 +0100

    Split function concerns: Remove the exchange order execution from the body of OrderService.create_order. Order creation just proxy the repository order creation.

--- backend/app/services/order_service.py
+++ backend/app/services/order_service.py
@@ -12,43 +12,49 @@ class OrderService:
     Handles order execution.
     """
 
-    def __init__(self, exchange: BinanceAdapter = None):
-        self.repository = OrderRepository() # creates its own session internally
+    def __init__(self, exchange: BinanceAdapter = None, repository: OrderRepository | None = None):
+        self.repository = repository or OrderRepository() # creates its own session internally
         self.exchange = exchange  # Can be None if no exchange configured
 
+
+    # @no more used exept in tests
     async def create_order(self, order: Order) -> Order:
+        """Create and persist order to DB"""
+        bot_logger.info(f"Creating order {order.side} {order.amount} {order.symbol}")
+
+        db_order = self.repository.create_order(order)
+        self.repository.close()
+        return db_order
+
+    async def execute_order(self, order_id) -> Order:
         """
-        Persist order to DB and execute it if a broker is configured.
+        Execute an order if a broker is configured.
         """
 
-        bot_logger.info(
-            f"Creating order {order.side} {order.amount} {order.symbol}",
-            extra={"symbol": order.symbol}
-        )
+        order = self.repository.get_order_by_id(order_id)
 
-        self.repository.create_order(order)
+        bot_logger.info(f"Executing order {order.side} {order.amount} {order.symbol}")
 
-        # Move this to a separate method
-        # Execute on exchange (optional)
         if self.exchange:
+            status = "EXECUTED"
             try:
                 if order.side == "BUY":
-                    await self.exchange.place_market_buy(order.symbol, order.amount)
+                    exchange_order = await self.exchange.place_market_buy(order.symbol, order.amount)
                 elif order.side == "SELL":
-                    await self.exchange.place_market_sell(order.symbol, order.amount)
-                # order.status = "EXECUTED"
+                    exchange_order = await self.exchange.place_market_sell(order.symbol, order.amount)
             except Exception as e:
-                # order.status = "FAILED"
+                status = "FAILED"
                 bot_logger.error(f"Binance Order Execution failed: {e}")
 
-            self.repository.update_order(
+            updated_order = self.repository.update_order(
                 id=order.id,
                 updates={
-                    "status": "EXECUTED",
+                    "status": status,
                     "executed_at": datetime.now()
+                    # "external_order_id": exchange_order.get("id")
                 }
             )
 
         self.repository.close()
 
-        return order
+        return updated_order

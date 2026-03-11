b6068cd60af0a6ea75fb1aea2f4b155339dd1a2d
Madalei <rizzolioestelle@gmail.com>
2026-03-11 13:24:25 +0100

    Replace hardcoded URL to be read from .env

--- backend/app/infrastructure/adapters/database.py
+++ backend/app/infrastructure/adapters/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # URL de connexion à PostgreSQL
-DATABASE_URL = "postgresql://botox_user:botoxine@localhost/botox"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://botox_user:botoxine@localhost/botox")
 
 # Création de l'engine SQLAlchemy
 engine = create_engine(DATABASE_URL)
